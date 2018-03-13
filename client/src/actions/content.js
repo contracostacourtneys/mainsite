985cf353822f17f615a42c88ae68fa19e1ee14cc
Xi Chen <XChen@contracosta.courts.ca.gov>
2018-03-13 14:48:37 -0700

    test jenkins #62

--- client/src/actions/content.js
+++ client/src/actions/content.js
@@ -12,16 +12,11 @@ import { STORE_STAGE_ID } from './types'
 // export const FETCH_POST = 'FETCH_POST';
 // export const FETCH_ASSET = 'FETCH_ASSET';
 
-import {
-  API_BASE_URL,
-  API_SPACE_ID,
-  API_TOKEN,
-  SMALL_CLAIMS_ID 
-} from process.env
-// const API_BASE_URL = 'https://cdn.contentful.com';
-// const API_SPACE_ID = 'x8bmio1z72gj';
-// const API_TOKEN = 'd5bbad9aaee8876cceb673a9de9364b29cf477d2ca96f978e684ba25f55ec74a';
-// const SMALL_CLAIMS_ID = '5iJkGCIR2gUoMKaeQOqo6W';
+const API_BASE_URL = process.env.API_BASE_URL;
+const API_SPACE_ID = process.env.API_SPACE_ID;
+const API_TOKEN = process.env.API_TOKEN;
+const SMALL_CLAIMS_ID = process.env.SMALL_CLAIMS_ID;
+console.log("client side process env", process.env);
 
 export function fetchCategories() {
   const request = axios.get(`${API_BASE_URL}/spaces/${API_SPACE_ID}/entries?access_token=${API_TOKEN}&content_type=category`);
