590be76b63d2518ca417b982dbdef0049ac9d93e
Xi Chen <XChen@contracosta.courts.ca.gov>
2018-03-13 14:53:49 -0700

    test Jenkins #63

--- webpack.config.js
+++ webpack.config.js
@@ -112,7 +112,12 @@ const config = {
     */
   },
   plugins: [
-    new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('production') } }),
+    new webpack.DefinePlugin({ 'process.env':  { NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+												API_BASE_URL: JSON.stringify(process.env.API_BASE_URL),
+  API_SPACE_ID: JSON.stringify(process.env.API_SPACE_ID),
+  API_TOKEN: JSON.stringify(process.env.API_TOKEN),
+  SMALL_CLAIMS_ID: JSON.stringify(process.env.SMALL_CLAIMS_ID) 
+											   } }),
     // new webpack.DefinePlugin({ 'process.env': { NODE_ENV: JSON.stringify('development') } }),
     // new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurrenceOrderPlugin(),
